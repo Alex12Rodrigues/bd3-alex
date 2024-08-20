af87721dabb23872be445a3c7f66844febf27e79
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2024-08-20 19:21:17 -0300

    DB3 coleção

---
+++ create-datbase.mongodb.js
@@ -0,0 +1,12 @@
+/*ME DO BANCO DE DADOS*/
+
+const database = 'db3-alex'
+
+/* NOME DO BANCO DA COLEÇÃO DE DAODS */
+const collection = "libvraria"
+
+/* CRIAR OU ACESSAR O BANCO DE DADOS */
+use (database);
+
+/* CRIAR UMA COLEÇÃO DE DADOS*/ 
+db.collection(collection);
