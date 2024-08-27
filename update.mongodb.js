c17d9cd7cbb556d2a06c8af2e3cc32bf02c5eb08
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2024-08-27 20:36:33 -0300

    Aumentando conhecimento de CRUD

---
+++ update.mongodb.js
@@ -0,0 +1,13 @@
+/*NOME DO BANCO DE DADOS*/
+const database = "BD3-Alex-Aula"
+
+/* NOME DO BANCO DA COLEÇÃO DE DADOS: */
+const collection = "livraria"
+
+/* CRIAR OU ACESSAR O BANCO DE DADOS: */
+use(database);
+
+/* ALTERA OS DADOS DE UM DOCUMENTO DA COLLECTION */
+db[collection].updateOne();
+
+
